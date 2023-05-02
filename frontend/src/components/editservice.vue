8b2814fc7e7e16a8b843191151ee7907d21aff58
dhuynh9 <89826984+dhuynh9@users.noreply.github.com>
2023-05-02 10:31:53 -0500

    Update editservice.vue

--- frontend/src/components/editservice.vue
+++ frontend/src/components/editservice.vue
@@ -19,6 +19,7 @@ export default {
       }
     },
   created() {
+  // get certain service info by sending GET request along with service id to backend
     axios.get(`${apiURL}/services/id/${this.$route.params.id}`).then((res) => {
       this.services = res.data
     })
